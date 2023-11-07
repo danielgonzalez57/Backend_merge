21cb165e5764694c261ca50015f8c8f6da50d8c2
christianqr12 <c.quintero@tiendasdaka.com>
2023-11-07 16:18:15 -0400

    Cambio Articulos

--- src/models/index.js
+++ src/models/index.js
@@ -1,11 +1,12 @@
 const { usersMergeSchema, UsersMerge } = require("./user.model");
-const { investigacionMergeSchema, investigacionMerge } = require("./investigacion.model");
 const { invesMergeSchema, InvesMerge } = require("./investigacionDos.model");
+const { articuloMergeSchema, MaestroArticuloMerge } = require("./articulo.model");
+
 
 function setupModels(sequelize) {
   UsersMerge.init(usersMergeSchema, UsersMerge.config(sequelize));
-  // investigacionMerge.init(investigacionMergeSchema, investigacionMerge.config(sequelize));
   InvesMerge.init(invesMergeSchema, InvesMerge.config(sequelize));
+  MaestroArticuloMerge.init(articuloMergeSchema, MaestroArticuloMerge.config(sequelize));
 }
 
 module.exports = setupModels;
