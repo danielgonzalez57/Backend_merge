77fbbb8e49b059780cd62da07d6cae2cae0eb598
christianqr12 <c.quintero@tiendasdaka.com>
2023-11-09 08:42:52 -0400

    Agregado modelo Linea

---
+++ src/models/linea.model.js
@@ -0,0 +1,52 @@
+// TABLA EO_PERSONA- PERSONAS
+const { Sequelize, Model, DataTypes } = require("sequelize");
+
+const tableName = "dim_LINEA_MERGE";
+const modelName = "modelLineaMerge";
+
+const LineaMergeSchema = {
+    
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  nombre: {
+    allowNull: false,
+    type: DataTypes.STRING(100),
+  },
+  cod_sap: {
+    allowNull: false,
+    type: DataTypes.STRING(20),
+  },
+  user_crea: {
+    allowNull: true,
+    type: DataTypes.INTEGER(70),
+  },
+  user_mod: {
+    allowNull: true,
+    type: DataTypes.INTEGER(70),
+  },
+};
+
+class MaestroLineaMerge extends Model {
+  static associate() {
+    // associate
+  }
+
+  static config(sequelize) {
+    return {
+      sequelize,
+      tableName: tableName,
+      modelName: modelName,
+      createdAt: "fec_crea",
+      updatedAt: "fec_mod"
+    };
+  }
+}
+
+module.exports = {
+  tableName,
+  LineaMergeSchema,
+  MaestroLineaMerge
+};
